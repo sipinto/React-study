cc294fe5f6c441c00c6a5ac0a251f20705046e76
sipinto <chairmankmj@gmail.com>
2023-10-05 16:46:28 +0900

    8 course complete styled-component

--- src/Component/Detail.js
+++ src/Component/Detail.js
@@ -1,4 +1,16 @@
 import { Link, useParams } from "react-router-dom";
+import styled from 'styled-components'
+
+let Box = styled.div`
+    padding : 20px;
+    color : grey
+`;
+let YellowBtn = styled.button`
+    background : yellow;
+    width : 120px;
+    color : black;
+    padding : 10px;
+`;
 
 function Detail(props) {
     let {id} = useParams();
@@ -9,6 +21,7 @@ function Detail(props) {
     // 디버깅용
     console.log(id);
     return(
+
         <div>
             <div className="container">
                 <div className="row">
@@ -17,13 +30,17 @@ function Detail(props) {
                     </div>
                     <div className="col-md-6">
                         <h4 className="pt-5">{found.title}</h4>
-                        <p>{found.content}</p>
+                        <Box>
+                            <p>{found.content}</p>
+                        </Box>
                         <p>{found.price}원</p>
                         <button className="btn btn-danger">주문하기</button> 
                     </div>
                 </div>
                 <Link to="/">
-                    <button>돌아가기</button>
+                    <Box>
+                        <YellowBtn>돌아가기</YellowBtn>
+                    </Box>
                 </Link>            
             </div> 
         </div>
